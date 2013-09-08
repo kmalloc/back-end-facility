44815f97cf409a56208f551476bc77ffbe8f5993
miliao <miliao@localhost.localdomain>
2013-09-08 15:23:54 +0800

    logging class

--- sys/src/log.cc
+++ sys/src/log.cc
@@ -1,23 +1,53 @@
 #include "log.h"
+#include <string>
+#include <fstream>
+#include <assert.h>
 
+using namespace std;
 
-class LogWorkerTask: public ITask
-{
-};
-
-Logger::Logger(size_t size, size_t granularity)
+Logger::Logger(const char* file, size_t size, size_t granularity)
     :m_size(size), m_granularity(granularity)
     ,m_buffer(m_size, m_granularity)
+    ,m_pendingMsg(m_size + 1)
+    ,m_logFile(file)
 {
+    sem_init(&m_sig, 0, 0);
+    ThreadBase::Start();
 }
 
 Logger::~Logger()
 {
+    assert(m_stopWorker);
 }
 
-void Logger::Init()
+void Logger::Run()
 {
+    while (!m_stopWorker)
+    {
+       sem_wait(&m_sig); 
 
+        void* buffer;
+
+        ofstream fout;
+        fout.open(m_logFile.c_str(), ofstream::out | ofstream::app);
+
+        if (!fout.good()) continue;
+
+        while (m_pendingMsg.Pop(&buffer))
+        {
+            fout << (char*)buffer;
+
+            m_buffer.ReleaseBuffer(buffer);
+        }
+
+        fout.close();
+    }
+}
+
+void Logger::StopLogging()
+{
+    m_stopWorker = true;
+    sem_post(&m_sig);
 }
 
 size_t Logger::Log(const char* msg)
